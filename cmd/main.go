28a907c71566c92266b2f04a4c490ed4919c233a
haruyama480 <haruyama480@gmail.com>
2024-03-23 02:39:12 +0900

    fix mysql

--- cmd/main.go
+++ cmd/main.go
@@ -2,42 +2,35 @@ package main
 
 import (
 	"context"
+	"database/sql"
 	"fmt"
 	"log"
 
-	"github.com/go-sql-driver/mysql"
+	"entgo.io/ent/dialect"
+	entsql "entgo.io/ent/dialect/sql"
+	_ "github.com/go-sql-driver/mysql"
 	"github.com/haruyama480/go-ent-learning/ent"
 )
 
 func main() {
-	// https://zenn.dev/masamiki/articles/83a8db3f132fcb1c48f0
-	entOptions := []ent.Option{}
+	dsn := "root:root@tcp(mysql:13306)/testdb?charset=utf8mb4&parseTime=True&loc=Local"
 
-	// 発行されるSQLをロギングするなら
-	entOptions = append(entOptions, ent.Debug())
-
-	// サンプルなのでここにハードコーディングしてます。
-	mc := mysql.Config{
-		User:                 "root",
-		Passwd:               "root",
-		Net:                  "tcp",
-		Addr:                 "127.0.0.1" + ":" + "13309", // localhost or dockerMySQL would be failed
-		DBName:               "testdb",
-		AllowNativePasswords: true,
-		ParseTime:            true,
-	}
-
-	datasource := mc.FormatDSN()
-	fmt.Println(datasource)
-	client, err := ent.Open("mysql", datasource, entOptions...)
+	db, err := sql.Open("mysql", dsn)
 	if err != nil {
-		log.Fatalf("Error open mysql ent client: %v\n", err)
+		log.Fatalf("failed opening connection to mysql: %v\n", err)
 	}
+	if err = db.Ping(); err != nil {
+		log.Fatalf("failed to ping db: %v\n", err)
+	}
+	fmt.Println("db connected!!")
 
+	// sql.DB -> entクライアントを作成
+	drv := entsql.OpenDB(dialect.MySQL, db)
+	client := ent.NewClient(ent.Driver(drv))
 	defer client.Close()
 
-	// Run the auto migration tool.
 	if err := client.Schema.Create(context.Background()); err != nil {
 		log.Fatalf("failed creating schema resources: %v", err)
 	}
+	fmt.Println("success!!")
 }
